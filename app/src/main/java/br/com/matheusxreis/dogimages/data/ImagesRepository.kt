6c39c6826472600e5a8e93a34811895de23baeb3
Matheus Reis <matheus.reis081@gmail.com>
2023-01-16 19:42:00 -0300

    feat: created api var in repository and consuming dog api

--- app/src/main/java/br/com/matheusxreis/dogimages/data/ImagesRepository.kt
+++ app/src/main/java/br/com/matheusxreis/dogimages/data/ImagesRepository.kt
@@ -1,13 +1,21 @@
 package br.com.matheusxreis.dogimages.data
 
 import android.util.Log
+import androidx.compose.ui.unit.Constraints
 import br.com.matheusxreis.dogimages.domain.entities.ImageData
 import br.com.matheusxreis.dogimages.domain.irepositories.IGetRandomImageRepository
+import br.com.matheusxreis.dogimages.utils.Api
+import br.com.matheusxreis.dogimages.utils.Constants
 
 class ImagesRepository: IGetRandomImageRepository {
+
+    lateinit private var api: IGetRandomImageRepository;
+
+    init {
+        api = Api.getApi(Constants.defaultBaseUrl).create(IGetRandomImageRepository::class.java)
+    }
     override suspend fun getRandomImageRepository(): ImageData {
-        val result = ImageData("url")
-        Log.d("im here", "")
+        val result = api.getRandomImageRepository()
         return result;
     }
 }
