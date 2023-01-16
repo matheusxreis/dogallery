f3c2cc325a630c073fb74b9d871e61ade9516968
Matheus Reis <matheus.reis081@gmail.com>
2023-01-16 18:47:38 -0300

    refactor: making repository GetRandomImage method as a suspend fun

--- app/src/main/java/br/com/matheusxreis/dogimages/domain/irepositories/GetRandomImageRepository.kt
+++ app/src/main/java/br/com/matheusxreis/dogimages/domain/irepositories/GetRandomImageRepository.kt
@@ -3,5 +3,5 @@ package br.com.matheusxreis.dogimages.domain.irepositories
 import br.com.matheusxreis.dogimages.domain.entities.ImageData
 
 interface GetRandomImageRepository {
-    fun getRandomImageRepository():ImageData
+    suspend fun getRandomImageRepository():ImageData
 }
