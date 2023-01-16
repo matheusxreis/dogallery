7dfea773ddf233daace0e47b28673f8d07b59d28
Matheus Reis <matheus.reis081@gmail.com>
2023-01-16 19:49:35 -0300

    feat: created first screen consuming api rightly

--- app/src/main/java/br/com/matheusxreis/dogimages/MainActivity.kt
+++ app/src/main/java/br/com/matheusxreis/dogimages/MainActivity.kt
@@ -3,14 +3,19 @@ package br.com.matheusxreis.dogimages
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import br.com.matheusxreis.dogimages.ui.theme.DogImagesTheme
+import br.com.matheusxreis.dogimages.ui.viewmodels.MainViewModel
+import coil.compose.AsyncImage
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -30,14 +35,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun MainScreen(mainViewModel: MainViewModel = viewModel()) {
+    var actualImageUrl = mainViewModel.actualImageUrl;
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DogImagesTheme {
-        Greeting("Android")
+    Column() {
+    if(actualImageUrl.length>0){
+        Text(text = "Actual image: ${mainViewModel.actualImageUrl}")
+        AsyncImage(
+            model=actualImageUrl,
+            contentDescription = null
+        )
+    }else {
+        Text(text="No image yet")
+    }
+
+    Button(onClick = { mainViewModel.getRandomImage() }) {
+        Text(text = "Buscar")
+    }
     }
 }
