125fdd7ad40d158c25de5857561e39cc75d01693
Matheus Reis <matheus.reis081@gmail.com>
2023-01-17 17:01:19 -0300

    feat: added db constants

--- app/src/main/java/br/com/matheusxreis/dogimages/utils/Constants.kt
+++ app/src/main/java/br/com/matheusxreis/dogimages/utils/Constants.kt
@@ -4,5 +4,10 @@ class Constants (){
 
     companion object {
         val defaultBaseUrl = "https://dog.ceo/api/"
+        val databaseName = "DogImages.db"
+        val imagesTableName = "Images"
+        val imagesColumnId = "id"
+        val imagesColumnUrl = "url"
+        val imagesColumnSavedAt = "saved_at"
     }
 }
