e4aedee1f28cd1d496202432cd554b6fd117bf55
Matheus Reis <matheus.reis081@gmail.com>
2023-01-17 19:33:36 -0300

    feat: making ImageStorageRepository implements its interface

--- app/src/main/java/br/com/matheusxreis/dogimages/data/repositories/ImageStorageRepository.kt
+++ app/src/main/java/br/com/matheusxreis/dogimages/data/repositories/ImageStorageRepository.kt
@@ -6,10 +6,11 @@ import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import android.util.Log
 import br.com.matheusxreis.dogimages.domain.entities.Image
+import br.com.matheusxreis.dogimages.domain.irepositories.ISaveImageInStorageRepository
 import br.com.matheusxreis.dogimages.utils.Constants
 import br.com.matheusxreis.dogimages.utils.DBHelper
 
-class ImageStorageRepository constructor(context:Context){
+class ImageStorageRepository constructor(context:Context):ISaveImageInStorageRepository{
 
     lateinit var  helper: SQLiteOpenHelper;
     lateinit var database: SQLiteDatabase;
@@ -19,7 +20,7 @@ class ImageStorageRepository constructor(context:Context){
         database = helper.writableDatabase
     }
 
-    fun saveImage(url:String){
+    override suspend fun saveImage(url:String){
         Log.d("SAVEEEEE", url)
         database.insert(
             Constants.imagesTableName,
