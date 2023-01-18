1ae45d6c498d83f38c3c08bbbca7e1de97090a5f
Matheus Reis <matheus.reis081@gmail.com>
2023-01-18 12:33:59 -0300

    feat: created sealed class of RootScreens

---
+++ app/src/main/java/br/com/matheusxreis/dogimages/ui/navigation/RootScreens.kt
@@ -0,0 +1,12 @@
+package br.com.matheusxreis.dogimages.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Favorite
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class RootScreens(val name:String, val icon: ImageVector){
+
+    object MAIN: RootScreens("main", Icons.Rounded.Search)
+    object GALLERY: RootScreens("gallery", Icons.Rounded.Favorite)
+}
