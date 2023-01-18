9d2201397b74204df19cd1000b6d6476ff415e60
Matheus Reis <matheus.reis081@gmail.com>
2023-01-18 11:11:44 -0300

    feat: done grid and dialog in gallery screen

--- app/src/main/java/br/com/matheusxreis/dogimages/ui/screens/GalleryScreen.kt
+++ app/src/main/java/br/com/matheusxreis/dogimages/ui/screens/GalleryScreen.kt
@@ -1,49 +1,110 @@
 package br.com.matheusxreis.dogimages.ui.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import br.com.matheusxreis.dogimages.ui.viewmodels.GalleryViewModel
 import coil.compose.AsyncImage
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.window.Dialog
+import br.com.matheusxreis.dogimages.ui.components.MyButton
 
 
 @Composable
 fun GalleryScreen(galleryViewModel: GalleryViewModel = viewModel()){
 
     val images = galleryViewModel.images;
+    var actualImage by remember {
+        mutableStateOf("")
+    }
+    var dialogState by remember {
+        mutableStateOf(false)
+    }
 
-    LaunchedEffect(Unit, {
+    fun openDialog(url:String){
+        actualImage = url
+        dialogState = true
+    }
+
+    LaunchedEffect(Unit) {
         galleryViewModel.getImages()
-    } )
+    }
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        images.map {
-            Text(text = it.id.toString())
-            AsyncImage(
-                model=it.url,
-                contentDescription = null,
-                contentScale = ContentScale.Crop,
-                modifier = Modifier
-                    .width(100.dp)
-                    .height(100.dp)
-                    .shadow(2.dp)
-                    .clip(
-                        RoundedCornerShape(2.dp)
-                    )
-            )
+    Column(modifier = Modifier.fillMaxWidth()) {
+
+
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 128.dp),
+            verticalArrangement = Arrangement.spacedBy(1.dp),
+            horizontalArrangement = Arrangement.spacedBy(1.dp),
+            modifier = Modifier
+                .background(color = MaterialTheme.colorScheme.background)
+                .fillMaxSize()
+        ) {
+            items(images) {
+                AsyncImage(
+                    model = it.url,
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    modifier = Modifier
+                        .width((50..300).random().dp)
+                        .height(300.dp)
+                        .clip(
+                            shape = RoundedCornerShape(2.dp)
+                        )
+                        .clickable {
+                            openDialog(it.url)
+                        }
+                )
+            }
         }
+
+        if(dialogState){
+            Dialog(onDismissRequest = { dialogState = false }) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .padding(horizontal = 2.dp)
+                            .background(color = Color.Transparent)
+                            .clickable { dialogState = false }
+                    ){
+                        AsyncImage(model = actualImage,
+                            contentDescription = "",
+                            modifier = Modifier.fillMaxWidth(1f)
+                           )
+                        Row(
+                            modifier = Modifier.fillMaxWidth(1f)
+                        ){
+
+                            MyButton(onClick = {},
+                                text = "Delete" ,
+                                icon = Icons.Rounded.Delete,
+                                color = MaterialTheme.colorScheme.error,
+                                enabled = true)
+
+                        }
+
+                    }
+
+
+            }
+        }
+
     }
 
 }
