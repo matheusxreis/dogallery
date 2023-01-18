82ae3ec7ddf6f15ff8460986c1c8631a2858ce3e
Matheus Reis <matheus.reis081@gmail.com>
2023-01-18 17:23:32 -0300

    feat: added login property in MyButton

--- app/src/main/java/br/com/matheusxreis/dogimages/ui/components/MyButton.kt
+++ app/src/main/java/br/com/matheusxreis/dogimages/ui/components/MyButton.kt
@@ -1,6 +1,8 @@
 package br.com.matheusxreis.dogimages.ui.components
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Icon
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -10,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun MyButton(
@@ -17,6 +20,7 @@ fun MyButton(
     text: String,
     icon: ImageVector,
     enabled: Boolean,
+    loading: Boolean = false,
     secondary: Boolean = false,
     color: Color? = null
 ){
@@ -26,8 +30,16 @@ fun MyButton(
             containerColor= color ?: if(secondary) Color.LightGray else MaterialTheme.colorScheme.primary,
         ),
         modifier = Modifier.fillMaxWidth()) {
-        Icon(icon, contentDescription = "", tint = MaterialTheme.colorScheme.background)
 
-        Text(text = text.uppercase())
+        if(loading){
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.background,
+                modifier = Modifier.width(30.dp)
+            );
+        }else {
+            Icon(icon, contentDescription = "", tint = MaterialTheme.colorScheme.background)
+            Text(text = text.uppercase())
+        }
+
     }
 }
